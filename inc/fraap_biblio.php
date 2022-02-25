bc01816f774a7c42187ba80647e67bc64e5de7de
christophe le drean <christophe@rouge-rose.net>
2022-02-25 15:10:54 +0100

    Synchronisation

--- inc/fraap_biblio.php
+++ inc/fraap_biblio.php
@@ -4,273 +4,305 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
+include_spip('base/abstract_sql');
+include_spip('action/editer_objet');
+include_spip('action/editer_liens');
+include_spip('inc/autoriser');
 
 function fraap_biblio_synchroniser($forcer = false) {
-	include_spip('inc/config');
-
-	$config_synchro = lire_config('fraap_biblio_synchro');
-	$config = lire_config('fraap_biblio');
-
-	if (isset($config_synchro['id_rubrique'])) {
-		$id_rubrique = $config_synchro['id_rubrique'];
-		$objet = 'rubrique';
-		$synchro['id_rubrique'] = $id_rubrique;
-	} else {
-		list($objet, $id_rubrique) = explode('|', $config['mediatheque'][0]);
-	}
-
-	// Vérifier que la configuration est complète
-	if ($id_rubrique == '' or $config['groupe'] == '') {
+	$config_synchro = fraap_biblio_synchro_configurer($forcer);
+	if (!$config_synchro) {
 		return [
 			'type' => 0,
-			'message' => _T('fraap_biblio:cfg_message_erreur_noconfig')
+			'message' => _T('fraap_biblio:cfg_message_erreur_noconfig'),
+		]; // Erreur, on arrête ici.
+	}
+
+	if ($config_synchro['fbiblios']['action'] == 'synchro') {
+		// Synchroniser les référénces (zitems -> fbiblios)
+		$synchro = fraap_biblio_synchroniser_fbiblios($forcer, $config_synchro);
+
+		if ($synchro == 1) {
+			// Passer à l'action suivante
+			$synchro = fraap_biblio_nettoyer_fbiblios();
+			// effacer la config et mettre à jour la date de synchro après l'étape nettoyage
+			ecrire_config('fraap_biblio_derniere_synchro', strval(strtotime('now')));
+			effacer_config('fraap_biblio_synchro');
+		}
+	}
+
+	if ($synchro < 0) {
+		return ['type' => -1]; // continuer la synchro
+	}
+	if ($synchro == 0) {
+		return [
+			'type' => 0,
+			'message' => _T('fbiblio:synchro_message_erreur'),
+		];
+	}
+	if ($synchro == 1) {
+		return ['type' => 1, 'message' => _T('fbiblio:synchro_message_ok')];
+	}
+}
+
+
+function fraap_biblio_nettoyer_fbiblios() {
+	$zitems = sql_allfetsel('id_zitem', 'spip_zitems', 'id_parent="0"');
+	$in = sql_in('statut', ['prepa', 'prop', 'publie']);
+	$fbiblios = sql_allfetsel('id_fbiblio, id_zitem', 'spip_fbiblios', $in);
+	$items = [];
+	$biblios = [];
+	foreach ($zitems as $zitem) {
+		$items[] = $zitem['id_zitem'];
+	}
+	foreach ($fbiblios as $fbiblio) {
+		$biblios[] = $fbiblio['id_zitem'];
+	}
+	$diff = array_diff($biblios, $items);
+
+	autoriser_exception('modifier', 'fbiblio', '*');
+	foreach ($diff as $id_zitem) {
+		$cle = array_search($id_zitem, array_column($fbiblios, 'id_zitem'));
+		$id_objet = $fbiblios[$cle]['id_fbiblio'];
+		objet_modifier('fbiblio', $id_objet, ['statut' => 'poubelle']);
+	}
+	autoriser_exception('modifier', 'fbiblio', '*', false);
+	return 1;
+}
+
+
+/**
+ *
+ * Contenu du tableau $config_synchro
+ * $config_synchro = [
+ *	'forcer' => true/false,
+ *	'mediatheque => ['id_objet' => '', 'objet' => ''],
+ *	'repertoire_mots' => [],
+ *	'nb_max_synchro' => '',
+ *	'date_derniere_synchro' => '',
+ *	'fbiblios' => [
+ *		'debut' => 0,
+ *		'offset' => nb_max_synchro
+ *		'etape' => 0,
+ *		'solde' => 0,
+ *		'total => 0,
+ *		'action' => '',
+ * ];
+ */
+function fraap_biblio_synchro_configurer($forcer) {
+	$config_plugin = lire_config('fraap_biblio');
+	$config_synchro = lire_config('fraap_biblio_synchro');
+	$derniere_synchro = intval(lire_config('fraap_biblio_derniere_synchro'));
+
+	$config_synchro = isset($config_synchro) ? $config_synchro : [];
+
+	$config_synchro['forcer'] = isset($config_synchro['forcer']) ? $config_synchro['forcer'] : $forcer;
+
+	// Rubrique Médiathèque
+	if (!isset($config_synchro['mediatheque'])) {
+		list($objet, $id_objet) = explode('|', $config_plugin['mediatheque'][0]);
+
+		if ($id_objet == '' or $config_plugin['groupe'] == '') {
+			return false;
+		} else {
+			$config_synchro['mediatheque'] = ['id_objet' => $id_objet, 'objet' => $objet];
+		}
+	}
+
+	// Répertoire des mots-clés
+	if (!isset($config_synchro['repertoire_mots'])) {
+		$config_synchro['repertoire_mots'] = fraap_biblio_constituer_repertoire_mots($config_plugin['groupe']);
+	}
+
+	if (!$config_synchro['repertoire_mots']) {
+		return false;
+	}
+
+	// Paliers de synchronisation
+	if (!isset($config_synchro['nb_max_synchro'])) {
+		$config_synchro['nb_max_synchro'] = $config_plugin['nb_max_synchro'];
+	}
+
+	// Config utilisée pour la synchro fbiblios
+	if (!isset($config_synchro['fbiblios'])) {
+		$config_synchro['fbiblios'] = [
+			'debut' => 0,
+			'offset' => $config_plugin['nb_max_synchro'],
+			'solde' => 0,
+			'etape' => 0,
+			'total' => 0,
+			'action' => 'synchro',
 		];
 	}
 
+	// Date de la dernière synchro
+	if (!isset($derniere_synchro)) {
+		$config_synchro['derniere_synchro'] = strtotime('now');
+		ecrire_config('fraap_biblio_derniere_synchro', strval($config_synchro['derniere_synchro']));
+	} else {
+		$config_synchro['derniere_synchro'] = $derniere_synchro;
+	}
+
+	return $config_synchro;
+}
+
+
+function fraap_biblio_synchroniser_fbiblios($forcer = false, $config = []) {
+	$in = sql_in('statut', ['prepa', 'prop', 'publie']);
+	/**
+	 * Après installation du plugin, la table fbiblios est vide.
+	 * On force la synchro.
+	 */
+	if (!$forcer and $config['fbiblios']['total'] == 0 and $total_fbiblios = intval(sql_countsel('spip_fbiblios', $in)) == 0) {
+		$forcer = true;
+		$config['forcer'] = $forcer;
+	}
+
 	if ($forcer) {
-		$synchro = ['forcer' => true];
+		$total_zitems = intval(sql_countsel('spip_zitems', 'id_parent="0"'));
+		$config['fbiblios']['solde'] = $total_zitems;
+		$config['fbiblios']['total'] = $total_zitems;
 	} else {
-		$synchro = isset($config_synchro) ? $config_synchro : ['forcer' => false];
+		if ($config['fbiblios']['total'] == 0 and $config['fbiblios']['etape'] == 0) {
+			$date_dernier_zitem = sql_getfetsel('updated', 'spip_zitems', 'id_parent="0"', '', 'updated DESC');
+
+			if ($date_dernier_zitem) {
+				$date_dernier_zitem = strtotime($date_dernier_zitem);
+				if ($date_dernier_zitem > $config['derniere_synchro']) {
+					$date_derniere_synchro = date('c', $config['derniere_synchro']);
+					$total_zitems = intval(sql_countsel('spip_zitems', 'id_parent="0" AND updated > ' . sql_quote($date_dernier_zitem)));
+					$config['fbiblios']['solde'] = $total_zitems;
+					$config['fbiblios']['total'] = $total_zitems;
+				}
+			}
+		}
 	}
 
-	if (isset($config_synchro['repertoire_mots'])) {
-		$repertoire_mots = $config_synchro['repertoire_mots'];
-	} else {
-		$repertoire_mots = fraap_biblio_constituer_repertoire_mots($config['groupe']);
-		$synchro['repertoire_mots'] = $repertoire_mots;
+	if ($config['fbiblios']['total'] > 0 and $config['fbiblios']['action'] == 'synchro') {
+		// Calculer le rang et le nombre de références à extraire à chaque étape
+		$config['fbiblios']['debut'] = $config['fbiblios']['offset'] * $config['fbiblios']['etape'];
+		$config['fbiblios']['solde'] = $config['fbiblios']['solde'] - $config['fbiblios']['offset'];
+		// Pas de solde négatif.
+		if ($config['fbiblios']['solde'] < 0) {
+			$config['fbiblios']['solde'] = 0;
+		}
+		$config['fbiblios']['etape'] += 1;
+
+		$limit = sql_quote($config['fbiblios']['debut']) . ',' . sql_quote($config['fbiblios']['offset']);
+		$zitems = sql_allfetsel('id_zitem, titre, auteurs', 'spip_zitems', 'id_parent="0"', '', 'updated DESC', $limit);
+
+		foreach ($zitems as $zitem) {
+			fraap_biblio_ajouter_fbiblios($zitem, $config);
+		}
 	}
 
-	$row = [
-		'repertoire_mots' => $repertoire_mots,
-		'nb_max_synchro' => $config['nb_max_synchro'],
-		'date_derniere_synchro' => isset($config['date_derniere_synchro']) ? $config['date_derniere_synchro'] : null,
+	if ($config['fbiblios']['solde'] > 0) {
+		// Ne plus forcer
+		$config['forcer'] = false;
+		ecrire_config('fraap_biblio_synchro', $config);
+		return -1; // continuer la synchro
+	} else {
+		// Solde à zero, nettoyer la table fbiblios
+		$config['fbiblios']['action'] = 'nettoyer';
+		ecrire_config('fraap_biblio_synchro', $config);
+		return 1;
+	}
+}
+
+
+function fraap_biblio_ajouter_fbiblios($zitem = [], $config = []) {
+	$in = sql_in('statut', ['prepa', 'prop', 'publie']);
+	$fbiblio = sql_fetsel('*', 'spip_fbiblios', 'id_zitem=' . sql_quote($zitem['id_zitem']) . ' AND ' . $in);
+	$fbiblio_set = [
+		'id_zitem' => $zitem['id_zitem'],
+		'titre' => $zitem['titre'],
+		'auteurs' => $zitem['auteurs'],
 	];
 
-	$traitement = fraap_biblio_synchroniser_fbiblios($id_rubrique, $row, $synchro['forcer']);
+	if (!$fbiblio) {
+		$fbiblio['id_fbiblio'] = objet_inserer('fbiblio', $config['mediatheque']['id_objet'], $fbiblio_set);
+	} else {
+		// mettre à jour la référence
+		autoriser_exception('modifier', 'fbiblio', $fbiblio['id_fbiblio']);
+		objet_modifier('fbiblio', $fbiblio['id_fbiblio'], $set);
+		autoriser_exception('modifier', 'fbiblio', $fbiblio['id_fbiblio'], false);
 
-	if ($traitement == 1) {
-		effacer_config('fraap_biblio_synchro');
-		return ['type' => 1, 'message' => _L('message_ok')];
+		// dissocier les mots-clés actuels
+		$fbiblio_mots = sql_allfetsel('id_mot', 'spip_mots_liens', 'objet=' . sql_quote('fbiblio') . ' AND id_objet=' . $fbiblio['id_fbiblio']);
+
+		if (count($fbiblio_mots) > 0) {
+			$ids = [];
+			foreach ($fbiblio_mots as $mot) {
+				$ids[] = $mot['id_mot'];
+			}
+			objet_dissocier(['mot' => $ids], ['fbiblio' => $fbiblio['id_fbiblio']]);
+		}
 	}
 
-	if ($traitement == 0) {
-		effacer_config('fraap_biblio_synchro');
-		return ['type' => 0, 'message' => _L('message erreur')];
+	$ztags = sql_allfetsel('tag', 'spip_ztags', 'id_zitem=' . sql_quote($zitem['id_zitem']));
+
+	$ajouter_mots = fraap_biblio_ajouter_mots($fbiblio['id_fbiblio'], $ztags, $config['repertoire_mots']);
+
+	$statut = null;
+
+	if ($ajouter_mots) {
+		if ($fbiblio['statut'] == 'prepa') {
+			$statut = ['statut' => 'prop'];
+		}
+	} else {
+		if ($fbiblio['statut'] == 'prop') {
+			$statut = ['statut' => 'prepa'];
+		}
+		if ($fbiblio['statut'] == 'publie') {
+			$statut = ['statut' => 'prepa'];
+		}
 	}
 
-	if ($traitement < 0) {
-		ecrire_config('fraap_biblio_synchro', $synchro);
-		return -1;
+	if ($statut) {
+		autoriser_exception('modifier', 'fbiblio', $fbiblio['id_fbiblio']);
+		objet_modifier('fbiblio', $fbiblio['id_fbiblio'], $statut);
+		autoriser_exception('modifier', 'fbiblio', $fbiblio['id_fbiblio'], false);
 	}
 }
 
 
+function fraap_biblio_ajouter_mots($id_fbiblio = 0, $ztags = [], $repertoire_mots = []) {
+	$res = ['index' => 0, 'categories' => 0, 'themes' => 0];
 
-function fraap_biblio_synchroniser_fbiblios($id_mediatheque = 0, $row = [], $forcer = false) {
-	include_spip('base/abstract_sql');
-	include_spip('action/editer_objet');
-	include_spip('action/editer_liens');
-	include_spip('inc/autoriser');
-
-	$config_synchro_fbiblios = lire_config('fraap_biblio_synchro_fbiblios');
-	// $fbiblio = [];
-	$repertoire_mots = $row['repertoire_mots'];
-
-	// Si aucune synchronisation connue, alors on force la mise à jour.
-	if (!isset($row['date_derniere_synchro'])) {
-		$forcer = true;
-		$row['date_derniere_synchro'] = strtotime('now');
-		ecrire_config('fraap_biblio/date_derniere_synchro', $row['date_derniere_synchro']);
+	if (intval($id_fbiblio) == 0) {
+		return false; // erreur
 	}
 
-	if (isset($config_synchro_fbiblios)) {
-		$synchro_fbiblios = $config_synchro_fbiblios;
-	} else {
-		$synchro_fbiblios = ['debut' => 0, 'offset' => $row['nb_max_synchro'], 'total' => 0, 'encours' => 0];
-	}
-
-	if ($forcer) {
-		// Nombre total des références à synchroniser
-		$nb_a_synchroniser = intval(sql_countsel('spip_zitems', 'id_parent="0"'));
-		$synchro_fbiblios['forcer'] = true;
-		$synchro_fbiblios['total'] = $nb_a_synchroniser;
-	} else {
-		$synchro_fbiblios['forcer'] = false;
-
-		// Si total est à 0, c'est le début de l'étape de synchro
-		// et vérifier combien de synchro à faire.
-		if ($synchro_fbiblios['total'] == 0 and $synchro_fbiblios['encours'] == 0) {
-			// La date de modification des références la plus récente
-			$dernier_zitem_maj = sql_getfetsel('updated', 'spip_zitems', 'id_parent="0"', '', 'updated DESC');
-
-			if ($dernier_zitem_maj) {
-				$date_derniere_synchro = $row['date_derniere_synchro'];
-				$date_dernier_zitem = strtotime($dernier_zitem_maj);
-
-				if ($date_dernier_zitem > $date_derniere_synchro) {
-					$date_iso_derniere_synchro = date('c', $date_derniere_synchro);
-					$nb_a_synchroniser = intval(sql_countsel('spip_zitems', 'id_parent="0" AND updated > ' . sql_quote($date_iso_derniere_synchro)));
-
-					// On ne synchronisera que les éléments nécessaires
-					if ($nb_a_synchroniser > 0) {
-						$synchro_fbiblios['total'] = $nb_a_synchroniser;
-					}
-				} else {
-					// Mettre à jour la date de synchro
-					$row['date_derniere_synchro'] = strtotime('now');
-					ecrire_config('fraap_biblio/date_derniere_synchro', $row['date_derniere_synchro']);
-
-					effacer_config('fraap_biblio_synchro_fbiblios');
-					// Rien à faire
-					return 1;
-				}
-			}
-		}
-	}
-
-	/**
-	 * Si le total est > à 50, on fractionne en plusieurs étapes.
-	 */
-	if ($synchro_fbiblios['offset'] < $synchro_fbiblios['total']) {
-		$synchro_fbiblios['total'] = $synchro_fbiblios['total'] - $synchro_fbiblios['offset'];
-	} else {
-		$synchro_fbiblios['offset'] = $synchro_fbiblios['total'];
-		$synchro_fbiblios['total'] = 0;
-	}
-
-	$synchro_fbiblios['encours'] += $synchro_fbiblios['offset'];
-
-	$limit = sql_quote($synchro_fbiblios['debut']) . ',' . sql_quote($synchro_fbiblios['offset']);
-
-	$zitems = sql_allfetsel('id_zitem, titre, auteurs', 'spip_zitems', 'id_parent="0"', '', 'updated DESC', $limit);
-
-	foreach ($zitems as $item) {
-		$synchro_tags = false;
-		$fbiblio = sql_fetsel('*', 'spip_fbiblios', 'id_zitem=' . sql_quote($item['id_zitem']));
-
-		$set = [
-			'id_zitem' => $item['id_zitem'],
-			'titre' => $item['titre'],
-			'auteurs' => $item['auteurs']
-		];
-
-		$ztags = sql_allfetsel('tag', 'spip_ztags', 'id_zitem=' . sql_quote($item['id_zitem']));
-
-		// Créer la référence bibliographique
-		if (intval($fbiblio['id_fbiblio']) == 0) {
-			$fbiblio['id_fbiblio'] = objet_inserer('fbiblio', $id_mediatheque, $set);
-
-			// TODO
-			// - nombre de tags liés à la référence
-			// - nombre de mots-clés liés effectivement à la référence
-			// => en fonction du résultat : publier ou dépublier ?
-
-			$synchro_tags = fraap_biblio_synchroniser_mots($fbiblio['id_fbiblio'], $ztags, $repertoire_mots);
-
-			/**
-			 * Si la synchro des tags est correcte,
-			 * alors on modifie le statut de prepa à prop
-			 */
-			if ($synchro_tags) {
-				autoriser_exception('modifier', 'fbiblio', $fbiblio['id_fbiblio']);
-				$mod = objet_modifier('fbiblio', $fbiblio['id_fbiblio'], ['statut' => 'prop']);
-				autoriser_exception('modifier', 'fbiblio', $fbiblio['id_fbiblio'], false);
-			}
-		} else {
-			// Modifier la référence déjà existante
-			autoriser_exception('modifier', 'fbiblio', $fbiblio['id_fbiblio']);
-			objet_modifier('fbiblio', $fbiblio['id_fbiblio'], $set);
-			autoriser_exception('modifier', 'fbiblio', $fbiblio['id_fbiblio'], false);
-
-			// Supprimer tous les liens existants entre fbiblio -> mots-clés
-			$mots = sql_allfetsel('id_mot', 'spip_mots_liens', 'objet=' . sql_quote('fbiblio') . ' AND id_objet=' . $fbiblio['id_fbiblio']);
-
-			if (count($mots) > 0) {
-				$ids = [];
-				foreach ($mots as $mot) {
-					$ids[] = $mot['id_mot'];
-				}
-				objet_dissocier(['mot' => $ids], ['fbiblio' => $fbiblio['id_fbiblio']]);
-			}
-
-			if (count($ztags) > 0) {
-				$synchro_mots = fraap_biblio_synchroniser_mots($fbiblio['id_fbiblio'], $ztags, $repertoire_mots);
-			}
-
-			/**
-			 * Modifier le statut ?
-			 * - si mots = true et si statut = prepa | prop => statut = prop
-			 * - si mots = true et si statut = publie => ne rien faire.
-			 * - si mots = false et si statut = prepa | prop => statut = prepa
-			 * - si mots = false et si statut = publie => statut = prepa
-			 * - pour les autres statuts = ne rien faire.
-			 */
-			$statut = null;
-			if ($synchro_mots) {
-				if ($fbiblio['statut'] == 'prepa' or $fbiblio['statut'] == 'prop') {
-					$statut = ['statut' => 'prop'];
-				}
-			} else {
-				if ($fbiblio['statut'] == 'prepa' or $fbiblio['statut'] == 'prop') {
-					$statut = ['statut' => 'prepa'];
-				}
-
-				if ($fbiblio['statut'] == 'publie') {
-					$statut = ['statut' => 'prepa'];
-				}
-			}
-
-			if ($statut) {
-				autoriser_exception('modifier', 'fbiblio', $fbiblio['id_fbiblio']);
-				objet_modifier('fbiblio', $fbiblio['id_fbiblio'], $statut);
-				autoriser_exception('modifier', 'fbiblio', $fbiblio['id_fbiblio'], false);
-			}
-		}
-	}
-	// Vérifier si des références restent à traiter
-	if ($synchro_fbiblios['total'] > 0) {
-		// Ne plus forcer
-		$synchro_fbiblios['forcer'] = false;
-		ecrire_config('fraap_biblio_synchro_fbiblios', $synchro_fbiblios);
-		return -1; // Poursuivre le traitement
-	} else {
-
-	}
-}
-
-
-
-function fraap_biblio_synchroniser_mots($id_fbiblio, $ztags = [], $repertoire_mots = []) {
-	$synchro = ['index' => 0, 'categories' => 0, 'themes' => 0];
 	if (is_array($ztags) and count($ztags) > 0) {
-		foreach ($ztags as $tag) {
-			$column = [];
+		foreach ($ztags as $ztag) {
+			$column = null;
 			$type_associer = '';
-			$tag_titre = fraap_biblio_normaliser_titre($tag['tag']);
-			preg_match('/(cat|mot)(=)/', $tag_titre, $matches);
+			$ztag_titre = fraap_biblio_normaliser_titre($ztag['tag']);
+
+			preg_match('/(cat|mot)(=)/', $ztag_titre, $matches);
+
 			if (count($matches) > 0) {
-				list($type, $titre) = explode('=', $tag_titre);
+				list($type, $titre) =  explode('=', $ztag_titre);
 
 				if ($type == 'cat') {
 					$column = $repertoire_mots['categories'];
 					$type_associer = 'categories';
-					// Ajouter au total attendu
-					$synchro['index'] += 1;
+					// Ajouter au total
+					$res['index'] += 1;
 				}
 
 				if ($type == 'mot') {
 					$column = $repertoire_mots['themes'];
 					$type_associer = 'themes';
-					// Ajouter au total attendu
-					$synchro['index'] += 1;
+					// Ajouter au total
+					$res['index'] += 1;
 				}
 
-				if (count($column) > 0) {
-					$cle = array_search($titre, array_column($column, 'titre')); // int ou null
+				if ($column) {
+					$cle = array_search($titre, array_column($column, 'titre'));
 					if ($cle) {
-						$post_association = objet_associer(['mot' => $column[$cle]['id_mot']], ['fbiblio' => $id_fbiblio]); // 0 ou 1
-						// Ajouter le résultat au tableau synchro
-						$synchro[$type_associer] += $post_association;
+						$associer = objet_associer(['mot' => $column[$cle]['id_mot']], ['fbiblio' => $id_fbiblio]);
+						$res[$type_associer] += $associer;
 					}
 				}
 			}
@@ -283,13 +315,12 @@ function fraap_biblio_synchroniser_mots($id_fbiblio, $ztags = [], $repertoire_mo
 	 * - au moins 1 mot-clé de type catégories a été ajouté
 	 * - le total attendu est égal au nombre de mots ajoutés
 	 */
-	if ($synchro['index'] > 0 and $synchro['categories'] > 0 and $synchro['index'] == $synchro['categories'] + $synchro['themes']) {
+	if ($res['index'] > 0 and $res['categories'] > 0 and $res['index'] == $res['categories'] + $res['themes']) {
 		return true;
 	} else {
 		return false;
 	}
 }
-
 
 
 function fraap_biblio_constituer_repertoire_mots($id_groupe) {
@@ -310,6 +341,10 @@ function fraap_biblio_constituer_repertoire_mots($id_groupe) {
 		} else {
 			$repertoire['themes'][] = $donnees;
 		}
+	}
+
+	if (count($repertoire['categories']) == 0 or count($repertoire['themes']) == 0) {
+		return false;
 	}
 
 	return $repertoire;
